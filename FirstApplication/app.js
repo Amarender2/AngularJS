fbc3944ff9553866c40eaae973c92bad20ac072f
armekala <ammv8@mst.edu>
2016-10-17 14:58:59 -0500

    First Angular App Settings

---
+++ FirstApplication/app.js
@@ -0,0 +1,11 @@
+(function() {
+  // So that we dont do mistakes unknowingly,
+  // will throw error if we use x = Hello;
+  'use strict'
+  
+  angular.module('myFirstApp',[])
+
+  .controller('myFirstController', function() {
+
+  })
+})();
