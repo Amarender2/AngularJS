76eb0b25864578793d1814690fac1491207490ee
armekala <ammv8@mst.edu>
2016-10-17 19:52:06 -0500

    Minification Proof for app.js

--- DependencyInjection/app.js
+++ DependencyInjection/app.js
@@ -1,12 +1,18 @@
 (function () {
 'use strict';
 
+// To make js minification proof, two methods
+// 1. Inline function with dependencies and function name as arguements
+// 2. function.$inject = ['', '']; dependencies
 angular.module('DIApp', [])
 .controller('DIController', DIController);
+// .controller('DIController', ['$scope', '$filter', '$injector', DIController]); - 1st method
 
+// DIController.$inject = ['$scope', '$filter', '$injector']; -- 2nd method
 // scope and filter injected
 // Angular JS uses $injector service to inject the dependencies needed in a function
 function DIController ($scope, $filter, $injector) {
+
   $scope.name = "Reddy";
 
   $scope.upper = function() {
