a340a356407479a816edd2cc4c7f6c5926cbb919
armekala <ammv8@mst.edu>
2016-11-08 17:02:21 -0600

    HTTP interceptor to show loading spinner

---
+++ Restaurant/src/common/loading/loading.interceptor.js
@@ -0,0 +1,53 @@
+(function() {
+"use strict";
+
+angular.module('common')
+.factory('loadingHttpInterceptor', LoadingHttpInterceptor);
+
+LoadingHttpInterceptor.$inject = ['$rootScope', '$q'];
+/**
+ * Tracks when a request begins and finishes. When a
+ * request starts, a progress event is emitted to allow
+ * listeners to determine when a request has been initiated.
+ * When the response completes or a response error occurs,
+ * we assume the request has ended and emit a finish event.
+ */
+function LoadingHttpInterceptor($rootScope, $q) {
+
+  var loadingCount = 0;
+  var loadingEventName = 'spinner:activate';
+
+  return {
+    request: function (config) {
+      // console.log("Inside interceptor, config: ", config);
+      // When request, parameters in config object, so set event
+      // loadingCount counts all request going out from app
+      if (++loadingCount === 1) {
+        $rootScope.$broadcast(loadingEventName, {on: true});
+      }
+
+      return config;
+    },
+
+    response: function (response) {
+      // loadingCount counts all responses received,
+      // until then we wont set the event false, so loading spinner will be shown till then
+      if (--loadingCount === 0) {
+        $rootScope.$broadcast(loadingEventName, {on: false});
+      }
+
+      return response;
+    },
+
+    responseError: function (response) {
+      if (--loadingCount === 0) {
+        $rootScope.$broadcast(loadingEventName, {on: false});
+      }
+
+      // reject must, or else caller will think response resolved successfully
+      return $q.reject(response);
+    }
+  };
+}
+
+})();
